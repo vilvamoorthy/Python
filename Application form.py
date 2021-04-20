e549d4c2b71e4175a236239a49cb1a6fd44a0231
hariharan-git <78402204+hariharan-git@users.noreply.github.com>
2021-04-20 13:25:08 +0530

    Create Application form.py

---
+++ Application form.py
@@ -0,0 +1,16 @@
+# enter the detail of the person:
+a = (input("Enter the name: "))
+b = int(input("Enter the age: "))
+c = int(input("Enter the employee number: "))
+d = (input("Enter the dept: "))
+
+# print function for gap between result:
+print()
+
+print("-----------Application-------------")
+print()
+
+print("Name of the person: ", a)
+print("Age of the person: ", b)
+print("Employee number of the person: ", c)
+print("Dept of the person: ",d)
