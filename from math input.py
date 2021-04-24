7ce36d0309bef7a903051974e90ac7a5b3d3cd5c
vdilipjayaraja <83122063+vdilipjayaraja@users.noreply.github.com>
2021-04-24 19:32:30 +0530

    Add files via upload

---
+++ from math input.py
@@ -0,0 +1,15 @@
+#mat functions 
+from math import*
+print(sqrt(36))#it will take suer roon of the valu
+print(floor(5.6))#it will round down the Number
+print(ceil(7.1))#it will round up the Number
+
+#list
+D=["dilip",5,56.78]
+print(D[2])#it will print the appropriate valu accoding to arry
+print(D[1:3])#it will print the appropriate valu accoding to arry by 1-3;starts from 1(5)and before 3,2(56.78)
+print(D[1:])#it will print the appropriate valu accoding to arry from 1
+print(D[:2])#it will print the appropriate valu accoding to arry upto2
+
+#functions of list 
+x=["vija",6,6.7]
---
+++ swaping.py
@@ -0,0 +1,8 @@
+print("this is the new program of dilip in python")
+A=int(input("enter your first value "))
+B=int(input("enter your second value "))
+print ('before swaping the values are ''a=',A,'and','b=',B)
+A=A+B
+B=A-B
+A=A-B
+print ('after swaping the values are ''a=',A,'and','b=',B)
