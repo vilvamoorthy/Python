4c6cf9ca4e3489449e4e1047e16bea3e621ff7ce
hariharan-git <78402204+hariharan-git@users.noreply.github.com>
2021-04-26 10:09:21 +0530

    Update Addition.py

--- Addition.py
+++ Addition.py
@@ -1,6 +1,9 @@
-x = int(input("Enter your First number"))
-y = int(input("Enter your Second number"))
+# Enter the x and y values:
+x = int(input("Enter your First number: "))
+y = int(input("Enter your Second number: "))
+
 def add(a,b):
     return a+b
+
 print(add(x,y))
 
